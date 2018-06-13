35b9c90c166ef30b1bc9fa16c5b4a9c55df1081e
NTRVanila <37998543+NTRVanila@users.noreply.github.com>
2018-06-13 19:46:46 +0700

    Add files via upload

---
+++ quot.sh
@@ -0,0 +1,224 @@
+#!/system/xbin/bash
+a="echo"
+clear
+figlet QuotesGen | lolcat
+sleep 0.8
+$a "  made with \033[31;5m ❤ \033[37;1m by \033[36;1m NTRVanila"
+$a "\033[93;1m"
+sleep 1
+$a "++++++++++++++++++++++++++++++++++++++"
+$a ""
+$a "Author : NTRVanila"
+$a "Github : https://github.com/NTRVanila"
+$a "Team   : BlackHole Security"
+$a "Fb     : facebook.com/rahyd.meleleh"
+$a "Contact: +6281223897972"
+$a ""
+$a "++++++++++++++++++++++++++++++++++++++"
+$a "\033[37;1m"
+sleep 0.8
+$a "  QuotesGen(?) Kumpulan quotes for bucin only"
+$a ""
+sleep 0.4
+$a "  list of perasaan"
+sleep 0.35
+$a "  ▶ sedih"
+sleep 0.5
+$a "  ▶ senang"
+sleep 0.7
+$a "  ▶ autis"
+$a ""
+sleep 1
+$a "  bagaimana perasaanmu sekarang sayang? ketikkan dibawah ya"
+$a ""
+read mrrm
+$a "\033[30;1m"
+if [ $mrrm = sedih ] || [ $mrrm = sedih ]
+then
+clear
+figlet Sedih | lolcat
+$a "\033[35;1m"
+sleep 5
+$a "Maaf Jika aku lancang menuliskan nama mu" 
+$a "disetiap lembar puisi yang kubuat."
+$a "Maaf jika diriku lancang juga"
+$a "menyebutkan nama mu disetiap doa ku."
+sleep 3
+$a "\033[31;1m"
+$a "Aku hanya sekedar terobsesi tentang mu, "
+$a "aku ingin menceritakan tentang bagaimana ku memperjuangkan mu. "
+$a "Bagai mana cara mu membuat ku terpikat, "
+$a "lalu sampai diriku mendapatkan dirimu." 
+$a "Aku hanya bangga akan dirimu karna tlah kumiliki."
+$a ""
+$a "Sekali lagi maaf."
+sleep 3
+$a "\033[34;1m"
+$a "Hanya dengan Diam dan memperhatikan"
+$a "aku bisa mendapatkan Kamu,"
+$a "apalagi dengan tindakan dan perlakuan ,"
+$a "mungkin kamu akan bersandar di bahuku selamanya"
+sleep 3
+$a "\033[32;1m"
+$a "Dari martabak. aku belajar."
+$a "bahwa yang dikacangin berarti special."
+sleep 5
+$a "\033[33;1m"
+$a "Jaman semakin gila,"
+$a "ada gengster, ada bom"
+$a "dan ada yg belom ketemu udah bilang sayang."
+sleep 5
+$a "\033[37;1m"
+$a "Tak seindah bayangmu.."
+$a "Tak seburuk realitamu,,"
+sleep 5
+$a "\033[35;1m"
+$a "Berhentilah berkata :"Semua cowok itu sama aja" "
+$a "Karena sampai kapanpun cowo manapun"
+$a "tidak akan pernah setara dengan gue :)"
+sleep 5
+$a "\033[32;1m"
+$a "bu dia nakal, kutuk dia"
+$a "jangan jadi batu tapi jadi kekasih ku"
+sleep 5
+$a "\033[36;1m"
+$a "Kalau cinta karena uang,"
+$a "Untuk kami orang-orang miskin"
+$a "apakah bisa merasakan cinta?"
+sleep 5
+$a "\033[35;1m"
+$a "minal aidzin wal faidzin,"
+$a "aku sayang kamu lahir dan batin"
+sleep 5
+$a "\033[34;1m"
+$a "nanti jika kau sudah bahagia ingat! ,"
+$a "aku pernah berjuang mengenggam eratmu"
+$a "melewati debat debat untuk melolakmu pergi:)"
+sleep 5
+$a "\033[34;1m"
+$a "nanti bila kau kembali dan aku sudah ada yang memiliki,"
+$a "aku harap kau mengerti."
+$a "menunggu tanpa kepastian, itu sangat melelahkan"
+sleep 5
+$a "\033[33;1m"
+$a "demi kamu aku akan bersabar menunggu"
+$a "dan karenamu aku menjadi orang yang penyabar."
+$a "Sabar menunggu tercapainya impianku bisa bersamamu"
+sleep 5
+$a "\033[32;1m"
+$a "harus kah aku mengulur waktu untuk mencintai mu ,"
+$a "Padahal sang kepastian tak kunjung datang menemuiku"
+sleep 5
+$a "\033[31;1m"
+$a "Kita bisa menyayangi & melupakan,"
+$a "Tapi terkadang kita tak ingin"
+$a "seseorang melupakan bahwa kita pernah berarti baginya. :)"
+sleep 5
+$a "\033[32;1m"
+$a "saat ini ku belum bener-bener bisa lupain semua,"
+$a "karena terlalu banyak kenangan manis"
+$a "yang udah aku laluin bareng sama kamu..."
+sleep 5
+$a "\033[33;1m"
+$a "Tak ada alasan untuk membenci,"
+$a "namun selalu belajar merelakan agar sakitku tak lebih lama"
+sleep 5
+$a "\033[34;1m"
+$a "ap benar lemak kalau lama gak dikasih kabar"
+$a "akhirnya jenuh juga?"
+sleep 5
+$a "\033[35;1m"
+$a "apa definisi tersakiti, "
+$a "selain melihatmu tertawa dengan yg lain"
+fi
+if [ $mrrm = senang ] || [ $mrrm = senang ]
+then
+clear
+figlet Senang | lolcat
+$a "\033[31;1m"
+$a "(Coming Soon)"
+$a "\033[37;1m"
+$a back to menu in 5 sec.
+sleep 4
+fi
+if [ $mrrm = autis ] || [ $mrrm = autis ]
+then
+clear
+figlet Autis | lolcat
+$a "\033[31;1m"
+$a "(Coming Soon)"
+$a "\033[37;1m"
+$a back to menu in 5 sec.
+sleep 4
+sh quot.sh
+fi
+if [ $mrrm = link ] || [ $mrrm = link ]
+then
+clear
+figlet LinkToolsIG | lolcat
+$a "\033[31;1m"
+$a step by step
+$a "\033[33;1m"
+sleep 0.5
+$a ▶ Pilih kemudian buka link
+sleep 0.3
+$a ▶ Login
+sleep 0.8
+$a ▶ Buka IG, tekan ini adalah saya
+sleep 0.7
+$a ▶ Ke web login lagi
+sleep 0.6
+$a ▶ Selamat njoki
+sleep 0.9
+$a   Jangan lupa untuk ganti password IG
+sleep 0.3 
+$a   untuk mencegah akunmu ngefollow orang gak jelas
+$a "\033[36;1m"
+sleep 8
+$a ""
+$a "+++++++++++++++++++++++++++++++++++++++++++++++++++"
+$a "\033[34;1m"
+$a http://bit.do/takpicee https://instagramtakipci.net/ 
+$a https://takipde.com/ 
+$a https://instagram.begenapp.net/ 
+$a https://www.instaturkiyem.com 
+$a http://takipcistar.com/ 
+$a https://sosyalmedyakazan.com/ 
+$a https://i.instagramtakiphilesi.com 
+$a https://takipci.instager.net 
+$a http://takipcikazandim.xyz/ 
+$a https://fenomencik.com/ 
+$a http://www.takipkanali.com/
+$a"" 
+$a DAFTAR APPLIKASINYA | lolcat
+$a "\033[34;1m"
+$a 1.http://files.appsgeyser.com/Go%20BOOSTER_5467242.apk 
+$a 1.http://instapopipanel.com/ 
+$a 2.http://takipcihanem.com/ 
+$a 3.http://freetakipci.com/tools/send-follower 4.https://instagramozel.com/tools/send-follower 5.https://sosyalmedyakazan.com/tools 
+$a 6.https://panelinsta.com/tools 
+$a 7.http://instagramservisleri.com/ 
+$a 8.http://takipcipanelim.com/ 
+$a 9.https://www.google.com/url?sa=t&source=web&rct=j&url=https://instagramtakipci.net/&ved=0ahUKEwiD-pnRoL_RAhUGopQKHZojDS8QFggbMAE&usg=AFQjCNGD0RaUJ0om4YbB8uM3zeKdprdUCw&sig2=ke9_2GbyAhvhi2GVtlhBTg 10.http://takipcikeyfi.com/tools/send-follower 11.https://instagramozel.com/tools/send-follower 12.http://bayinsta.com/ 
+$a 13.https://instagramozel.com/tools/send-follower 14.http://www.instafenomeni.net/ 
+$a 15.https://takipgram.com/ 
+$a 16.https://instagramozel.com/tools/send-follower 17.https://sosyalmedyakazan.com/ 
+$a 18.http://www.instafenomeni.net/ 
+$a 19.https://begendirsene.com/ 
+$a 20.https://www.google.com/url?sa=t&source=web&rct=j&url=https://instagramtakipci.net/&ved=0ahUKEwiD-pnRoL_RAhUGopQKHZojDS8QFggbMAE&usg=AFQjCNGD0RaUJ0om4YbB8uM3zeK.dprdUCw&sig2=ke9_2GbyAhvhi2GVtlhBTg 21.https://takipcipanelim.com/#77VfFY3eXSbqGWsL.99 22.https://takipci.instager.net/ 
+$a 23.https://takipji.com/ 
+$a 24.http://instagram.begenapp.com/ 
+$a 25.http://www.instagramtakipcihilesi.xyz/
+$a 26.http://www.instagram.begenihilesi.com/
+$a 27.http://instalihecim.com/ 
+$a 28.http://organiktakipci.com/ 
+$a 29.http://www.instagram-takipci-satin-al.com/
+$a 30.https://kotusozluk.com/instagram-takipci-arttirma--1945853 
+$a 31.http://instagramaraci.com/ 
+$a 32.http://instakipci.begen.co/ 
+$a 33.http://instapopim.com/ 
+$a 34.https://www.instagramtakiphilesi.com/
+$a 35.https://www.takipcikazaniyorum.com/ 
+$a 36.http://takipcihanem.com/ 
+$a 37.http://s1.seononline.com/
+fi
